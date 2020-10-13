8814b6c70f5691832e7da68f5520e9940f1d030e
Bobor77 <leco.emanuel731@gmail.com>
2020-10-13 16:20:52 +0200

    added basic state properties to subject

--- src/Subject.jsx
+++ src/Subject.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import './Subject.css'
 
 class Subject extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: props.name,
+            color: this.state.classMap[this.state.name],
+            classMap: {
+                odpN: "net",
+                pos: "net",
+                cps: "net",
+                cvm: "mat",
+                mat: "mat",
+                sjl: "sjl",
+                tsv: "tsv",
+                anj: "anj",
+                kaj: "anj",
+                fyz: "fyz",
+                eko: "eko",
+                sgp: "sgp",
+                odpG: "odpG",
+                cas: "pro",
+                asw: "pro",
+                pro: "pro",
+                sos: "fun",
+                cio: "fun",
+                obn: "obn"
+            }
+        } 
+    }
+
     render() {
-        return <td></td>
+    
+        /*this.setState({
+
+        });
+    */
+        return <td className={this.state.color}>{this.state.name}</td>
     }
 
 }
