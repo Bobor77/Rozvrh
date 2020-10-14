72c7f578aac2250657fe27654925d9260312520b
Bobor77 <leco.emanuel731@gmail.com>
2020-10-14 11:14:32 +0200

    Updates

---
+++ precache-manifest.ff0541087fc850f8b0a94bc4bd3b6e23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d053c64fe993fa7e8aaa8fafc9e7af23",
+    "url": "/rozvrh/index.html"
+  },
+  {
+    "revision": "37ae2161fc3df38d3c83",
+    "url": "/rozvrh/static/css/main.f856ff54.chunk.css"
+  },
+  {
+    "revision": "5031aa0223a130496281",
+    "url": "/rozvrh/static/js/2.2167a04d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rozvrh/static/js/2.2167a04d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37ae2161fc3df38d3c83",
+    "url": "/rozvrh/static/js/main.675999c8.chunk.js"
+  },
+  {
+    "revision": "659672daea62c06ae7d6",
+    "url": "/rozvrh/static/js/runtime-main.1188e7b1.js"
+  }
+]);
