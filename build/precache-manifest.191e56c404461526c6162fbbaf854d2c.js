925ea9a40778f30954714f938ae60317912686a0
Bobor77 <57676929+Bobor77@users.noreply.github.com>
2020-10-14 11:08:21 +0200

    added build file for github pages

---
+++ build/precache-manifest.191e56c404461526c6162fbbaf854d2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ce11a9d5a78f502223cb79a6bfa38c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37ae2161fc3df38d3c83",
+    "url": "/static/css/main.f856ff54.chunk.css"
+  },
+  {
+    "revision": "5031aa0223a130496281",
+    "url": "/static/js/2.2167a04d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2167a04d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37ae2161fc3df38d3c83",
+    "url": "/static/js/main.675999c8.chunk.js"
+  },
+  {
+    "revision": "936d848a5a98e3cd5594",
+    "url": "/static/js/runtime-main.23bd8a9c.js"
+  }
+]);
